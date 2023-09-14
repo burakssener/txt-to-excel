3eff766aa735fe9da45a4c69d412276eb1afd2ec
burakssener <ssenerburak@gmail.com>
2023-09-14 21:05:35 +0300

    this version collects all txt files from subdirectories with running once

--- exceler3.0.py
+++ exceler3.0.py
@@ -1,26 +1,25 @@
 import pandas as pd
-import glob
 import os
 
-folder_path = 'C:/Users/burak/Desktop/instaloader/babybearyuki'  # Replace with your directory path
-txt_files = glob.glob(f"{folder_path}/*.txt")
+# Define the parent directory containing subdirectories with text files
+parent_directory = 'C:/Users/burak/Desktop/instaloader'
 
-def load_file(filename: str) -> list:
-    result = []
-    file_texts = []  # Store text lines for each file
-    with open(filename, encoding="utf-8") as infile:
-        for line in infile.read().splitlines():
-            if line.strip():  # Check if the line is not empty
-                file_texts.append(line)  # Collect non-empty lines
-    result.append([os.path.basename(filename), "\n".join(file_texts)])  # Include the file name and combined text
-    return result
-
-# Concatenate the data from all files into a single list
+# Initialize an empty list to store data
 data = []
-for filename in txt_files:
-    data.extend(load_file(filename))
 
+# Iterate through subdirectories and their files using os.walk
+for root, dirs, files in os.walk(parent_directory):
+    for file in files:
+        if file.endswith(".txt"):  # Check if the file has a .txt extension
+            file_path = os.path.join(root, file)  # Get the full file path
+            with open(file_path, encoding="utf-8") as infile:
+                text = infile.read()
+                data.append([file, text])
+
+# Create a Pandas DataFrame
+df = pd.DataFrame(data, columns=["File Name", "Text"])
+
+# Save the data to an Excel file
 with pd.ExcelWriter("output.xlsx") as writer:
     sheet_name = "combined_data"
-    df = pd.DataFrame(data, columns=["File Name", "Text"])
     df.to_excel(writer, sheet_name=sheet_name, index=False)
